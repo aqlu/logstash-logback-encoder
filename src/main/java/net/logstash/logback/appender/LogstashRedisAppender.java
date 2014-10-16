0a26aa1a6b7e4d8077a2707a489113d48a8a9ce5
angus.aqlu <angus.aqlu@gmail.com>
2014-10-16 09:35:41 +0800

    add batch output and  refactor when output to redis failed then output to another appender

--- src/main/java/net/logstash/logback/appender/LogstashRedisAppender.java
+++ src/main/java/net/logstash/logback/appender/LogstashRedisAppender.java
@@ -14,36 +14,78 @@
 package net.logstash.logback.appender;
 
 import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.Appender;
 import ch.qos.logback.core.Layout;
 import ch.qos.logback.core.UnsynchronizedAppenderBase;
+import ch.qos.logback.core.spi.AppenderAttachable;
+import ch.qos.logback.core.spi.AppenderAttachableImpl;
 import net.logstash.logback.layout.LogstashLayout;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 import redis.clients.jedis.Protocol;
 
+import java.util.*;
+import java.util.concurrent.*;
+
 /**
  * Created by aqlu on 2014-10-13.
  * output log to redis
  * <p>
  * Configurations example:
+ * 
  * <pre>
  * &lt;appender name="redisAppender" class="net.logstash.logback.appender.LogstashRedisAppender"&gt;
- *     &lt;host&gt;172.19.65.153&lt;/host&gt;
- *     &lt;port&gt;6379&lt;/port&gt;
- *     &lt;key&gt;logstash_intf_log&lt;/key&gt;
+ *     &lt;host&gt;172.19.65.153&lt;/host&gt;          &lt;!-- redis host, required --&gt;
+ *     &lt;port&gt;6379&lt;/port&gt;                   &lt;!-- redis port, default is 6379 --&gt;
+ *     &lt;key&gt;logstash_intf_log&lt;/key&gt;        &lt;!-- redis key, required --&gt;
+ *     &lt;enableBatch&gt;true&lt;/enableBatch&gt;     &lt;!-- true is batch write to redis; default is true --&gt;
+ *     &lt;batchSize&gt;100&lt;/batchSize&gt;          &lt;!-- batch size, default 100 --&gt;
+ *     &lt;period&gt;500&lt;/period&gt;                &lt;!-- each write period redis, default 500 ms --&gt;
  *     &lt;layout class="net.logstash.logback.layout.LogstashLayout"&gt;
  *         &lt;includeContext&gt;false&lt;/includeContext&gt;
  *         &lt;includeMdc&gt;false&lt;/includeMdc&gt;
  *         &lt;includeCallerInfo&gt;false&lt;/includeCallerInfo&gt;
  *         &lt;customFields&gt;{"source": "your source", "type": "your type"}&lt;/customFields&gt;
  *     &lt;/layout&gt;
+ *     &lt;appender-ref ref="fileAppender" /&gt;       &lt;!-- output to this appender when output to redis failed; if you are not configured, then ignore this log, optional; --&gt;
+ * &lt;/appender&gt;
+ * 
+ * &lt;appender name="fileAppender" class="ch.qos.logback.core.rolling.RollingFileAppender"&lt;
+ *     &lt;file&gt;/you/log/path/log-name.ing&lt;/file&lt;
+ *     &lt;rollingPolicy class="ch.qos.logback.core.rolling.TimeBasedRollingPolicy"&gt;
+ *         &lt;fileNamePattern&gt;/you/log/path/log-name.%d{yyyy-MM-dd_HHmm}.log.%i&lt;/fileNamePattern&gt;
+ *         &lt;MaxHistory&gt;30&lt;/MaxHistory&gt;
+ *     &lt;/rollingPolicy&gt;
+ *     &lt;encoder class="net.logstash.logback.encoder.LogstashEncoder"&gt;
+ *         &lt;includeContext&gt;false&lt;/includeContext&gt;
+ *         &lt;includeMdc&gt;false&lt;/includeMdc&gt;
+ *         &lt;includeCallerInfo&gt;false&lt;/includeCallerInfo&gt;
+ *         &lt;customFields&gt;{"source": "your source", "type": "your type"}&lt;/customFields&gt;
+ *     &lt;/encoder&gt;
  * &lt;/appender&gt;
  * </pre>
+ * 
  * </p>
  */
-public class LogstashRedisAppender extends UnsynchronizedAppenderBase<ILoggingEvent>  {
+public class LogstashRedisAppender extends UnsynchronizedAppenderBase<ILoggingEvent> implements
+        AppenderAttachable<ILoggingEvent>, Runnable {
+
+    private AppenderAttachableImpl<ILoggingEvent> aai = new AppenderAttachableImpl<ILoggingEvent>();
+
+    private Queue<ILoggingEvent> events = new ConcurrentLinkedQueue<ILoggingEvent>();
+
+    private Layout<ILoggingEvent> layout = new LogstashLayout();
+
+    private ScheduledExecutorService executor;
+
+    private int messageIndex = 0;
+
+    private int appenderCount = 0;
+
     private JedisPool pool;
+
+    /* configurable params */
 
     private String host;
 
@@ -57,22 +99,78 @@ public class LogstashRedisAppender extends UnsynchronizedAppenderBase<ILoggingEv
 
     private String password;
 
-    private Layout<ILoggingEvent> layout = new LogstashLayout();
+    private int batchSize = 100;
+
+    private long period = 500; // ms
+
+    private boolean enableBatch = true;
+
+    private boolean daemonThread = true;
+
+    public void run() {
+        String[] batchLogs = new String[batchSize];
+        ILoggingEvent[] batchEvents = new ILoggingEvent[batchSize];
+
+        try {
+            ILoggingEvent event;
+            while ((event = events.poll()) != null) {
+                batchEvents[messageIndex] = event;
+                batchLogs[messageIndex] = layout.doLayout(event);
+                messageIndex++;
+
+                if (messageIndex >= batchSize) {
+                    push(batchLogs);
+                }
+            }
+
+            if (messageIndex > 0) {
+                push(Arrays.copyOf(batchLogs, messageIndex));
+            }
+
+        } catch (Exception e) {
+            addInfo("record log to redis failed.", e);
+            if (appenderCount > 0) {
+                this.appendLoopOnAppenders(Arrays.copyOf(batchEvents, messageIndex));
+            }
+        } finally {
+            messageIndex = 0;
+        }
+    }
 
     @Override
-    protected void append(ILoggingEvent eventObject) {
+    protected void append(ILoggingEvent event) {
+        try {
+            if (enableBatch) {
+                events.add(event);
+            } else {
+                push(layout.doLayout(event));
+            }
+        } catch (Exception e) {
+            addInfo("record log to redis failed.", e);
+
+            if (appenderCount > 0) {
+                this.appendLoopOnAppenders(event);
+            }
+
+        }
+    }
+
+    /**
+     * push log to redis
+     * @param jsonLogs logs for json format
+     */
+    private void push(String... jsonLogs) throws Exception {
         Jedis client = null;
         try {
             client = pool.getResource();
-            String json = layout.doLayout(eventObject);
-            client.rpush(key, json);
+            client.rpush(key, jsonLogs);
         } catch (Exception e) {
-            addInfo("record log to redis failed.");
-
             if (client != null) {
                 pool.returnBrokenResource(client);
                 client = null;
             }
+
+            throw e;
         } finally {
             if (client != null) {
                 pool.returnResource(client);
@@ -80,15 +178,25 @@ public class LogstashRedisAppender extends UnsynchronizedAppenderBase<ILoggingEv
         }
     }
 
+    private void appendLoopOnAppenders(ILoggingEvent... events) {
+        for (ILoggingEvent event : events) {
+            this.aai.appendLoopOnAppenders(event);
+        }
+    }
+
     @Override
     public void start() {
         super.start();
         pool = new JedisPool(new JedisPoolConfig(), host, port, timeout, password, database);
+
+        executor = Executors.newSingleThreadScheduledExecutor(new NamedThreadFactory("RedisAppender", daemonThread));
+        executor.scheduleWithFixedDelay(this, period, period, TimeUnit.MILLISECONDS);
     }
 
     @Override
     public void stop() {
         super.stop();
+        executor.shutdown();
         pool.destroy();
     }
 
@@ -154,5 +262,74 @@ public class LogstashRedisAppender extends UnsynchronizedAppenderBase<ILoggingEv
 
     public void setLayout(Layout<ILoggingEvent> layout) {
         this.layout = layout;
+    }
+
+    public int getBatchSize() {
+        return batchSize;
+    }
+
+    public void setBatchSize(int batchSize) {
+        this.batchSize = batchSize;
+    }
+
+    public long getPeriod() {
+        return period;
+    }
+
+    public void setPeriod(long period) {
+        this.period = period;
+    }
+
+    public boolean isEnableBatch() {
+        return enableBatch;
+    }
+
+    public void setEnableBatch(boolean enableBatch) {
+        this.enableBatch = enableBatch;
+    }
+
+    public boolean isDaemonThread() {
+        return daemonThread;
+    }
+
+    public void setDaemonThread(boolean daemonThread) {
+        this.daemonThread = daemonThread;
+    }
+
+    @Override
+    public void addAppender(Appender<ILoggingEvent> newAppender) {
+        appenderCount++;
+        addInfo("Attaching appender named [" + newAppender.getName() + "] to LogstashRedisAppender.");
+        aai.addAppender(newAppender);
+    }
+
+    @Override
+    public Iterator<Appender<ILoggingEvent>> iteratorForAppenders() {
+        return aai.iteratorForAppenders();
+    }
+
+    @Override
+    public Appender<ILoggingEvent> getAppender(String name) {
+        return aai.getAppender(name);
+    }
+
+    @Override
+    public boolean isAttached(Appender<ILoggingEvent> appender) {
+        return aai.isAttached(appender);
+    }
+
+    @Override
+    public void detachAndStopAllAppenders() {
+        aai.detachAndStopAllAppenders();
+    }
+
+    @Override
+    public boolean detachAppender(Appender<ILoggingEvent> appender) {
+        return aai.detachAppender(appender);
+    }
+
+    @Override
+    public boolean detachAppender(String name) {
+        return aai.detachAppender(name);
     }
 }
